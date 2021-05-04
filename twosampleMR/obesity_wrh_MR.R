b667ef964123f84dfd93cf790465c28359809429
samvidav <samvida.venkatesh@gmail.com>
2021-05-04 18:38:01 +0100

    Add unadjusted pvalues and Q-relative comparison

--- twosampleMR/obesity_wrh_MR.R
+++ twosampleMR/obesity_wrh_MR.R
@@ -24,7 +24,7 @@ harmonised <- harmonise_data(exposures, outcomes)
 IVW <- mr(harmonised, method_list = "mr_ivw")
 IVW <- generate_odds_ratios(IVW)
 # Adjust pval for multiple testing
-IVW$pval <- p.adjust(IVW$pval, method = "fdr")
+IVW$adj_pval <- p.adjust(IVW$pval, method = "fdr")
 # Check heterogeneity
 IVW_het <- mr_heterogeneity(harmonised, method_list = "mr_ivw")
 # Merge 
@@ -34,7 +34,7 @@ IVW <- merge(IVW, IVW_het)
 egger <- mr(harmonised, method_list = "mr_egger_regression")
 egger <- generate_odds_ratios(egger)
 # Adjust pval for multiple testing
-egger$pval <- p.adjust(egger$pval, method = "fdr")
+egger$adj_pval <- p.adjust(egger$pval, method = "fdr")
 # Check heterogeneity
 egger_het <- mr_heterogeneity(harmonised, method_list = "mr_egger_regression")
 # Merge 
@@ -42,32 +42,36 @@ egger <- merge(egger, egger_het)
 
 # Merge with IVW results
 all_MR <- IVW[, c("outcome", "exposure", "nsnp", "or", "or_lci95", "or_uci95", 
-                  "pval", "Q", "Q_pval")]
-colnames(all_MR)[4:9] <- c("OR_IVW", "LCI_IVW", "UCI_IVW", "pval_IVW", 
-                           "Q_IVW", "Qpval_IVW")
+                  "pval", "adj_pval", "Q", "Q_pval")]
+colnames(all_MR)[4:10] <- c("OR_IVW", "LCI_IVW", "UCI_IVW", 
+                            "pval_IVW", "adj_pval_IVW",
+                            "Q_IVW", "Qpval_IVW")
 all_MR <- merge(all_MR, egger[, c("outcome", "exposure", "nsnp", "or", "or_lci95",
-                                  "or_uci95",  "pval", "Q")])
-colnames(all_MR)[10:14] <- c("OR_Egger", "LCI_Egger", "UCI_Egger", 
-                             "pval_Egger", "Q_Egger")
+                                  "or_uci95",  "pval", "adj_pval", "Q", "Q_pval")])
+colnames(all_MR)[11:17] <- c("OR_Egger", "LCI_Egger", "UCI_Egger", 
+                             "pval_Egger", "adj_pval_Egger",
+                             "Q_Egger", "Qpval_Egger")
 
 # Check if MR-Egger is significantly less heterogeneous
 
-all_MR$diff_Q <- all_MR$Q_IVW - all_MR$Q_Egger
-all_MR$Qpval_egger <- pchisq(all_MR$diff_Q, df = 1)
+all_MR$Q_relative <- all_MR$Q_Egger/all_MR$Q_IVW 
 
 # Method to use
-all_MR$method <- ifelse(all_MR$Qpval_IVW >= 0.05, "IVW", "Egger")
+all_MR$method <- ifelse(all_MR$Qpval_IVW >= 0.05, "IVW", 
+                        ifelse(all_MR$Qpval_Egger >= 0.05 & all_MR$Q_relative < 0.5, "Egger", 
+                               "IVW"))
 
 # 3. Weighted median for sensitivity
 wtmed <- mr(harmonised, method_list = "mr_weighted_median")
 wtmed <- generate_odds_ratios(wtmed)
 # Adjust pval for multiple testing
-wtmed$pval <- p.adjust(wtmed$pval, method = "fdr")
+wtmed$adj_pval <- p.adjust(wtmed$pval, method = "fdr")
 
 all_MR <- merge(all_MR, wtmed[, c("outcome", "exposure", "or", "or_lci95", "or_uci95",
-                                  "pval")])
-colnames(all_MR)[18:21] <- c("OR_weighted_median", "LCI_weighted_median", 
-                             "UCI_weighted_median", "pval_weighted_median")
+                                  "pval", "adj_pval")])
+colnames(all_MR)[20:24] <- c("OR_weighted_median", "LCI_weighted_median", 
+                             "UCI_weighted_median", "pval_weighted_median",
+                             "adj_pval_weighted_median")
 
 write.table(all_MR, paste(PATH, 
                           "/results/obesity_meta_wrh_mr_results_280720.txt", 
