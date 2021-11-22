e7bbe427ac61f8a9f82c3e322c1df53caee6fa93
Samvida S. Venkatesh <samvida.venkatesh@gmail.com>
2021-11-22 10:50:30 +0000

    Add unadjusted logistic regression
    By editor's request, PLOS Med

--- observational_ukbb/logit_regression.R
+++ observational_ukbb/logit_regression.R
@@ -34,27 +34,27 @@ data$menopause_factor <- factor(ifelse(data$menopause_status == 0, "No",
                                                      "Not sure", NA))))
 # BMI adjusted:
 BMImodel <- glm(BMI ~ age + age_sq + assessment_centre + smoking_factor,
-               data = data,
-               na.action = na.exclude)
+                data = data,
+                na.action = na.exclude)
 data$adjBMI <- residuals(BMImodel)
 data$adjBMI <- qnorm((rank(data$adjBMI, na.last = "keep") - 0.5) / 
                        sum(!is.na(data$adjBMI)))
 
 # WHR adjusted:
 WHRmodel <- glm(WHR ~ age + age_sq + assessment_centre + smoking_factor,
-               data = data,
-               na.action = na.exclude)
+                data = data,
+                na.action = na.exclude)
 data$adjWHR <- residuals(WHRmodel)
 data$adjWHR <- qnorm((rank(data$adjWHR, na.last = "keep") - 0.5) / 
                        sum(!is.na(data$adjWHR)))
 
 # WHRadjBMI adjusted:
 WHRadjBMImodel <- glm(WHR ~ BMI + age + age_sq + assessment_centre + smoking_factor,
-               data = data,
-               na.action = na.exclude)
+                      data = data,
+                      na.action = na.exclude)
 data$WHRadjBMI <- residuals(WHRadjBMImodel)
 data$WHRadjBMI <- qnorm((rank(data$WHRadjBMI, na.last = "keep") - 0.5) / 
-                       sum(!is.na(data$WHRadjBMI)))
+                          sum(!is.na(data$WHRadjBMI)))
 
 # Run logistic regression ----
 
@@ -64,9 +64,9 @@ diagnoses <- c("endometriosis", "excessive_menstruation", "infertility",
 
 BMI_model_fits <- lapply(diagnoses, function (d) {
   m <- glm(formula = paste(d, " ~ adjBMI", sep = ""),
-      data = data, 
-      na.action = na.exclude,
-      family = binomial(link = "logit"))
+           data = data, 
+           na.action = na.exclude,
+           family = binomial(link = "logit"))
   beta <- summary(m)$coefficients[, 1][2]
   sd <- summary(m)$coefficients[, 2][2]
   pval <- summary(m)$coefficients[, 4][2]
@@ -80,9 +80,9 @@ BMI_model_fits <- bind_rows(BMI_model_fits)
 
 WHR_model_fits <- lapply(diagnoses, function (d) {
   m <- glm(formula = paste(d, " ~ adjWHR", sep = ""),
-      data = data, 
-      na.action = na.exclude,
-      family = binomial(link = "logit"))
+           data = data, 
+           na.action = na.exclude,
+           family = binomial(link = "logit"))
   beta <- summary(m)$coefficients[, 1][2]
   sd <- summary(m)$coefficients[, 2][2]
   pval <- summary(m)$coefficients[, 4][2]
@@ -96,9 +96,9 @@ WHR_model_fits <- bind_rows(WHR_model_fits)
 
 WHRadjBMI_model_fits <- lapply(diagnoses, function (d) {
   m <- glm(formula = paste(d, " ~ WHRadjBMI", sep = ""),
-      data = data, 
-      na.action = na.exclude,
-      family = binomial(link = "logit"))
+           data = data, 
+           na.action = na.exclude,
+           family = binomial(link = "logit"))
   beta <- summary(m)$coefficients[, 1][2]
   sd <- summary(m)$coefficients[, 2][2]
   pval <- summary(m)$coefficients[, 4][2]
@@ -117,13 +117,89 @@ all$adj_pval <- p.adjust(all$pval, method = "fdr")
 write.table(all, paste(PATH, "results/logistic_regression_smoking_results_200921.txt", sep = ""),
             sep = "\t", quote = F, row.names = F)
 
+# Models for unadjusted regressions ----
+
+diagnoses <- c("endometriosis", "excessive_menstruation", "infertility",
+               "miscarriage", "PCOS", "preeclampsia.or.eclampsia", 
+               "uterine_fibroids")
+
+# BMI only:
+data$rint_BMI <- qnorm((rank(data$BMI, na.last = "keep") - 0.5) / 
+                         sum(!is.na(data$BMI)))
+
+# WHR only:
+data$rint_WHR <- qnorm((rank(data$WHR, na.last = "keep") - 0.5) / 
+                         sum(!is.na(data$WHR)))
+
+# WHRadjBMI only:
+WHRadjonlyBMImodel <- glm(WHR ~ BMI,
+                          data = data,
+                          na.action = na.exclude)
+data$WHRadjonlyBMI <- residuals(WHRadjonlyBMImodel)
+data$WHRadjonlyBMI <- qnorm((rank(data$WHRadjonlyBMI, na.last = "keep") - 0.5) / 
+                              sum(!is.na(data$WHRadjonlyBMI)))
+
+BMI_model_fits <- lapply(diagnoses, function (d) {
+  m <- glm(formula = paste(d, " ~ rint_BMI", sep = ""),
+           data = data, 
+           na.action = na.exclude,
+           family = binomial(link = "logit"))
+  beta <- summary(m)$coefficients[, 1][2]
+  sd <- summary(m)$coefficients[, 2][2]
+  pval <- summary(m)$coefficients[, 4][2]
+  d <- data.frame(obesity_trait = "BMI", diagnosis = d, 
+                  OR = exp(beta), 
+                  LCI = exp(beta) - 1.96*sd, UCI = exp(beta) + 1.96*sd,
+                  pval = pval)
+  return (d)
+})
+BMI_model_fits <- bind_rows(BMI_model_fits)
+
+WHR_model_fits <- lapply(diagnoses, function (d) {
+  m <- glm(formula = paste(d, " ~ rint_WHR", sep = ""),
+           data = data, 
+           na.action = na.exclude,
+           family = binomial(link = "logit"))
+  beta <- summary(m)$coefficients[, 1][2]
+  sd <- summary(m)$coefficients[, 2][2]
+  pval <- summary(m)$coefficients[, 4][2]
+  d <- data.frame(obesity_trait = "WHR", diagnosis = d, 
+                  OR = exp(beta), 
+                  LCI = exp(beta) - 1.96*sd, UCI = exp(beta) + 1.96*sd,
+                  pval = pval)
+  return (d)
+})
+WHR_model_fits <- bind_rows(WHR_model_fits)
+
+WHRadjBMI_model_fits <- lapply(diagnoses, function (d) {
+  m <- glm(formula = paste(d, " ~ WHRadjonlyBMI", sep = ""),
+           data = data, 
+           na.action = na.exclude,
+           family = binomial(link = "logit"))
+  beta <- summary(m)$coefficients[, 1][2]
+  sd <- summary(m)$coefficients[, 2][2]
+  pval <- summary(m)$coefficients[, 4][2]
+  d <- data.frame(obesity_trait = "WHRadjBMI", diagnosis = d, 
+                  OR = exp(beta), 
+                  LCI = exp(beta) - 1.96*sd, UCI = exp(beta) + 1.96*sd,
+                  pval = pval)
+  return (d)
+})
+WHRadjBMI_model_fits <- bind_rows(WHRadjBMI_model_fits)
+
+all <- bind_rows(BMI_model_fits, WHR_model_fits, WHRadjBMI_model_fits)
+
+all$adj_pval <- p.adjust(all$pval, method = "fdr")
+
+write.table(all, paste(PATH, "results/logistic_regression_unadjusted_results_211122.txt", sep = ""),
+            sep = "\t", quote = F, row.names = F)
 
 # Models for comparative body size at age 10 (categorical variable) ----
 
 # Create variables for body size 
 data$body_size_vars <- factor(ifelse(data$body_size_10 == 1, "Thinner",
-                                       ifelse(data$body_size_10 == 2, "Plumper",
-                                              ifelse(data$body_size_10 == 3, "Average", NA))))
+                                     ifelse(data$body_size_10 == 2, "Plumper",
+                                            ifelse(data$body_size_10 == 3, "Average", NA))))
 
 body_size_fits <- lapply(diagnoses, function (d) {
   m <- glm(formula = paste(d, " ~ body_size_vars", sep = ""),
